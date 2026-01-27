c3d9e9650777ce799baf3b943eb1193375b96e78
edu <edu@192.168.0.35>
2026-01-27 18:12:29 +0900

    6day_fin

---
+++ spring_crud_exc5_machida/src/main/resources/static/js/sort_empid.js
@@ -0,0 +1,38 @@
+let isAsc = true;
+
+function sort() {
+	//	テーブル情報を取得
+	let employee = document.querySelectorAll('list_table');
+	//行情報を取得
+	let emprow = employee.querySelectorAll('tr');
+
+	let employeeList = [];
+	//	配列に行情報を追加
+	for (let i = 1; i < emprow.length; i++) {
+		employeeList.push(emprow[i]);
+	}
+
+	//昇順並び変える
+	employeeList.sort((a, b) => {
+		let numA = Number(a.cells[0].textContent);
+		let numB = Number(b.cells[0].textContent);
+		return numA - numB;
+	});
+	if (!isAsc) {
+		employeeList.reverse();
+
+	}
+	
+	isAsc=!isAsc;
+	//	降順に並び変える
+	//	employeeList.sort((a, b) => {
+	//		return b - a;
+	//	});
+	for (let i = 0; i < employeeList.length; i++) {
+		employee.appendChild(employeeList[i]);
+
+	}
+
+
+
+}
